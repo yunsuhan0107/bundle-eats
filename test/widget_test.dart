27852c86c13620d72282d41c1aae1392bacb2b06
jcha-dot <twin1907@gmail.com>
2022-07-15 01:19:50 +0900

    -m 'changed widget_test'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:new_project/main.dart';
+import 'package:bundle_eats/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
