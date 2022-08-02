4b479b9d2e57d06a6bae3c4d1cffae9f2f22ae8c
Yoonjae Hwang <yoonjae3@illinois.edu>
2022-08-02 21:41:19 +0900

    settings

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -114,108 +114,6 @@ class Menus {
   });
 }
 
-// class SpecificProfile extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white.withOpacity(.94),
-//       appBar: AppBar(
-//         title: Text(""),
-//         centerTitle: true,
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(10),
-//         child: ListView(
-//           children: [
-//             // user card
-//             SimpleUserCard(
-//               userName: "상세 프로필",
-//               userProfilePic: AssetImage(""),
-//             ),
-//             SettingsGroup(
-//               items: [
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: CupertinoIcons.pencil_outline,
-//                   iconStyle: IconStyle(),
-//                   title: '프로필',
-//                   subtitle: "개인정보",
-//                 ),
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: Icons.fingerprint,
-//                   iconStyle: IconStyle(
-//                     iconsColor: Colors.white,
-//                     withBackground: true,
-//                     backgroundColor: Colors.red,
-//                   ),
-//                   title: '보안',
-//                   subtitle: "",
-//                 ),
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: Icons.dark_mode_rounded,
-//                   iconStyle: IconStyle(
-//                     iconsColor: Colors.white,
-//                     withBackground: true,
-//                     backgroundColor: Colors.red,
-//                   ),
-//                   title: 'Dark mode',
-//                   subtitle: "Automatic",
-//                   trailing: Switch.adaptive(
-//                     value: false,
-//                     onChanged: (value) {},
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             SettingsGroup(
-//               items: [
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: Icons.info_rounded,
-//                   iconStyle: IconStyle(
-//                     backgroundColor: Colors.purple,
-//                   ),
-//                   title: '',
-//                   subtitle: "",
-//                 ),
-//               ],
-//             ),
-//             // You can add a settings title
-//             SettingsGroup(
-//               settingsGroupTitle: "Account",
-//               items: [
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: Icons.exit_to_app_rounded,
-//                   title: "로그아웃",
-//                 ),
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: CupertinoIcons.repeat,
-//                   title: "계정변경 씨발러마",
-//                 ),
-//                 SettingsItem(
-//                   onTap: () {},
-//                   icons: CupertinoIcons.delete_solid,
-//                   title: "계정삭제 개새꺄",
-//                   titleStyle: TextStyle(
-//                     color: Colors.red,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -264,7 +162,7 @@ class SettingsPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text("환경설정"),
         ), //AppBar
-        body: const UserSettings()); //scaffold
+        body: UserSettings()); //scaffold
   }
 }
 
--- lib/screens/settings/user_settings.dart
+++ lib/screens/settings/user_settings.dart
@@ -1,12 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
 
-class UserSettings extends StatelessWidget {
+class UserSettings extends StatefulWidget {
   const UserSettings({Key? key}) : super(key: key);
 
   @override
+  State<UserSettings> createState() => _UserSettings();
+}
+
+class _UserSettings extends State<UserSettings> {
+  final List<bool> _switchValues = List.generate(8, (_) => false);
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Text'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              '알림',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ListTileSwitch(
+              value: _switchValues[0],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[0] = value;
+                });
+              },
+              subtitle: const Text('쿠폰 및 마케팅 정보 알림'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '이벤트 혜택',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[1],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[1] = value;
+                });
+              },
+              subtitle: const Text('리뷰 쓰기 알림 (22시~08시 미발송)'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '리뷰 쓰기',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[2],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[2] = value;
+                });
+              },
+              subtitle: const Text('내 리뷰에 달린 댓글 알림 (22시~08시 미발송)'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '리뷰 댓글',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[3],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[3] = value;
+                });
+              },
+              subtitle: const Text('배달 상태 실시간 알림'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '배달 현황',
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              '기능',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ListTileSwitch(
+              value: _switchValues[4],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[4] = value;
+                });
+              },
+              subtitle: const Text('전화주문 시 가게번호 주소록에 저장'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '가게번호 자동 저장',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[5],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[5] = value;
+                });
+              },
+              subtitle: const Text('최대 5대의 휴대폰에서 동시에 로그인 할 수 있도록 허용'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '여러 기기에서 로그인 허용',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[6],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[6] = value;
+                });
+              },
+              subtitle: const Text('개인정보보호를 위해 실제 전화번호 대신 가상의 번호를 사용'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '안심번호 항상 사용',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTileSwitch(
+              value: _switchValues[7],
+              toggleSelectedOnValueChange: true,
+              leading: const Icon(Icons.access_alarms),
+              onChanged: (value) {
+                setState(() {
+                  _switchValues[7] = value;
+                });
+              },
+              subtitle: const Text('서비스 및 마케팅 프로모션에 활용할 수 있도록 허용'),
+              switchActiveColor: Colors.blueGrey,
+              title: const Text(
+                '개인정보 수집 이용 동의',
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
